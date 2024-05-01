a7b896b52cf0c3e8017bf2626ff6cfe17452bd73
Henrique Bonatto <89865580+NaTToHen@users.noreply.github.com>
2024-04-30 21:55:11 -0300

    services controller

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ import express, {Request, Response} from 'express';
 import databaseConnection from './utils/database';
 
 import dotenv from 'dotenv';
+import UsuarioController from './controllers/UsuarioController';
 dotenv.config()
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello!');
 })
+
+app.use("/user", UsuarioController)
 
 app.listen(PORT, async () => {
     console.log('Listening on port', PORT);
