cf62940c1dce6a99ae5bdb4f10748ea884c4b279
Pedro Lucas Dall Igna <pedro.lucas-di@hotmail.com>
2024-05-04 22:54:41 -0300

    feat: adds basic error handling

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -7,26 +7,33 @@ import httpStatus from "http-status";
 const route = Router();
 
 route.post('/', async (req: Request, res: Response) => {
-    if (req.body) {
-        const data: IUser = {
-            nome: req.body.nome,
-            email: req.body.email,
-            idade: req.body.idade,
-            genero: req.body.genero,
-            telefone: req.body.telefone,
-            cpf: req.body.cpf,
-            rg: req.body.rg
-        };
-        const user = await createUser(data);
-
-        return res.status(httpStatus.CREATED).send(user);
+    try {
+        if (req.body) {
+            const data: IUser = {
+                nome: req.body.nome,
+                email: req.body.email,
+                idade: req.body.idade,
+                genero: req.body.genero,
+                telefone: req.body.telefone,
+                cpf: req.body.cpf,
+                rg: req.body.rg
+            };
+            const user = await createUser(data);
+    
+            return res.status(httpStatus.CREATED).json(user);
+        }
+    } catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
     }
 });
 
 route.get('/', async (req: Request, res: Response) => {
-    const users = await getAllUsers();
-
-    return res.status(httpStatus.OK).send(users);
+    try {
+        const users = await getAllUsers();
+        return res.status(httpStatus.OK).json(users);
+    } catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+    }
 });
 
 route.get('/:id', async (req: Request, res: Response) => {
@@ -34,8 +41,8 @@ route.get('/:id', async (req: Request, res: Response) => {
         const {id: userId} = req.params;
         const user = await getUserById(userId);
         return res.status(httpStatus.OK).send(user);
-    } catch (err) {
-        return res.status(httpStatus.NOT_FOUND);
+    } catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
     }
 });
 
@@ -52,9 +59,9 @@ route.put('/:id', async (req: Request, res: Response) => {
             rg: req.body.rg
         };
         const user = await updateUserById(userId, data);
-        return res.status(httpStatus.OK).send(user);
-    } catch (err) {
-        return res.status(httpStatus.NOT_FOUND);
+        return res.status(httpStatus.OK).json(user);
+    } catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
     }
 });
 
@@ -62,9 +69,9 @@ route.delete('/:id', async (req: Request, res: Response) => {
     try {
         const {id: userId} = req.params;
         const user = await deleteUserById(userId);
-        return res.status(httpStatus.OK).send(user);
-    } catch (err) {
-        return res.status(httpStatus.NOT_FOUND);
+        return res.status(httpStatus.OK).json(user);
+    } catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
     }
 });
 
